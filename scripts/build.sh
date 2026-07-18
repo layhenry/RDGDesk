be4312ecac496deb1c0bffbb3e34842df947a276
layhenry <layhenry@users.noreply.github.com>
2026-07-18 13:11:04 +0800

    fix: support default build script invocation

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,8 +8,8 @@ ln -s "$prefix" "$pkgconfig_alias_root/prefix"
 trap 'rm -f "$pkgconfig_alias_root/prefix"; rmdir "$pkgconfig_alias_root"' EXIT
 export PKG_CONFIG_PATH="$pkgconfig_alias_root/prefix/lib/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}"
 export DYLD_LIBRARY_PATH="$prefix/lib${DYLD_LIBRARY_PATH:+:$DYLD_LIBRARY_PATH}"
-swift_build_arguments=()
 if [[ "${RDC_SWIFTPM_DISABLE_SANDBOX:-0}" == "1" ]]; then
-  swift_build_arguments+=(--disable-sandbox)
+  swift build --disable-sandbox
+else
+  swift build
 fi
-swift build "${swift_build_arguments[@]}"
